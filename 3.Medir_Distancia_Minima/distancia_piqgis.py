1f5d15afa0b90b470754d41232ed932947789cd4
Larissa Valadão <111463081+larissavaladao@users.noreply.github.com>
2026-07-30 11:36:32 -0300

    plano de código
    Added comments for clarity on distance measurement process.

--- 3.Medir_Distancia_Minima/distancia_piqgis.py
+++ 3.Medir_Distancia_Minima/distancia_piqgis.py
@@ -1,1 +1,13 @@
-
+# referencia  https://git.disroot.org/jpcasares/medir_distancia_entre.git
+# selecionar layers
+  # ADA - apenas um polígono / sítios - multipolígono ou multiponto
+# definir projeção de medida - ibge
+  # checar/reprojetar para a projeção  
+# filtrar por distância
+  # se sitios são pontos filtrar sitios a 600 metros da ADA
+  # se sítios são polígonos filtras sitios a 300 da ADA
+    # se o numero de geometrias do filtro for 0 
+      #printar que não há sitios arquelogicos proximos que justifiquem analise manual
+    # se o numero de geometrias for > 0:
+      # calcular menor distancia entre sitios/ ADA
+        #printar a distancia e o nome dos sítios
